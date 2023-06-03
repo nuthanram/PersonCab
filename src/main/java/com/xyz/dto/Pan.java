e735b7e413d6eb64fbdf1f22d4dcb65b763535e7
Nuthan Reddy <nuthan.ram.509@gmail.com>
2023-06-03 12:24:05 +0530

    pan done

--- src/main/java/com/xyz/dto/Pan.java
+++ src/main/java/com/xyz/dto/Pan.java
@@ -1,5 +1,39 @@
 package com.xyz.dto;
 
-public class Pan {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Pan {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String panref;
+	private String DOB;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPanref() {
+		return panref;
+	}
+
+	public void setPanref(String panref) {
+		this.panref = panref;
+	}
+
+	public String getDOB() {
+		return DOB;
+	}
+
+	public void setDOB(String dOB) {
+		DOB = dOB;
+	}
 }
